60f4780e38a562ca86d77d308d0fff1dba5c3966
namdev26 <vuhoangnam310104@gmail.com>
2025-02-01 12:33:14 +0700

    ore

--- _Data/Item/ItemCode.cs
+++ _Data/Item/ItemCode.cs
@@ -2,8 +2,7 @@ public enum ItemCode {
     NoItem = 0,
     IronOre = 1,
     GoldOre = 2,
-    // CopperOre = 3,
-    // SilverOre = 4,
-    // Coal = 5,
-    // IronIngot = 6,
+    CoalOre = 3,
+    DiamondOre = 4,
+    EmeraldOre = 5,
 }
